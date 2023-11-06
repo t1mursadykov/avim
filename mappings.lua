ec199e352f8229ca09a6f7022e7bc4a753eff4c6
Садыков Тимур <sdkv.tmr@gmail.com>
2023-11-06 15:47:47 +0300

    fix go-to preview behavior

--- mappings.lua
+++ mappings.lua
@@ -2,40 +2,28 @@ return {
   n = {
     -- Go To preview
     ["gpd"] = {
-      function ()
-        require('goto-preview').goto_preview_definition()
-      end,
-      desc = "Preview goto definition"
+      function() require("goto-preview").goto_preview_definition { focus_on_open = false, dismiss_on_move = true } end,
+      desc = "Preview goto definition",
     },
 
     ["gpc"] = {
-      function ()
-        require('goto-preview').close_all_win()
-      end,
-      desc = "Close all goto preview windows"
+      function() require("goto-preview").close_all_win() end,
+      desc = "Close all goto preview windows",
     },
 
     ["gpt"] = {
-      function ()
-        require('goto-preview').goto_preview_type_definition()
-      end,
-      desc = "Type definition"
+      function() require("goto-preview").goto_preview_type_definition { focus_on_open = false, dismiss_on_move = true } end,
+      desc = "Type definition",
     },
 
     ["gpi"] = {
-      function ()
-        require('goto-preview').goto_preview_implementation()
-      end,
-      desc = "Implementation"
+      function() require("goto-preview").goto_preview_implementation { focus_on_open = false, dismiss_on_move = true } end,
+      desc = "Implementation",
     },
 
     ["gpr"] = {
-      function ()
-        require('goto-preview').goto_preview_references()
-      end,
-      desc = "References"
+      function() require("goto-preview").goto_preview_references { focus_on_open = false, dismiss_on_move = true } end,
+      desc = "References",
     },
-
-
-  }
+  },
 }
